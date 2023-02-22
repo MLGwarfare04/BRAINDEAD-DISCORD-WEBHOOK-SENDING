6fe51742896d55494d52b853535c62d550660de2
MLGwarfare04 <89049597+MLGwarfare04@users.noreply.github.com>
2023-02-21 20:16:15 -0700

    made shorter

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express")
-const bodyParser = require('body-parser')
-const fetch = require("node-fetch")
-const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { WebhookClient } = require('discord.js');
 
 const webhookClient = new WebhookClient({url:process.env.hook1}); // hook1 is the webhook url
 const AUTHKEY = process.env['KEY'] // an auth key so people cant just fire randomly, any random string
 
 const server = express()
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(express.json());
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('server running on', process.env.PORT || 3000)
@@ -18,24 +15,24 @@ function isrblx(req) { // for rolbox people, remove this func and the func call 
   return req.headers['roblox-id'] && req.headers['user-agent'] // check if the person is on roblox
 }
 
-async function requesturl(body,embeds) {
+async function requesturl(body) {
 // unused stuff lazy to add
   const response = webhookClient.send(body)
+  return response
 }
 
 
-server.get('/', (req, res) => {
+server.get('/', (_, res) => {
   res.send(
 `failure of life`
   )
 })
 
 server.post("/", (req, res) => {
-  console.log(req.body,req.headers.url)
   if(req.headers.auth && req.headers.auth == AUTHKEY && isrblx(req)){
     console.log("sending request")
     var response = requesturl(req.body)
-    res.status(200).send({Message: "success"})
+    res.status(200).send({Message: "success",Response: response})
   }else{
     res.status(401).send({Message: "failure of life"})
   }
